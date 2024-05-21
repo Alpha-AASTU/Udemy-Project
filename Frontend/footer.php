88f5b0617104437e014b85e585921cab98716269
Mahider Jemere <124486265+Mahi-gitlex@users.noreply.github.com>
2024-05-22 01:12:17 +0300

    Create footer.php

---
+++ Frontend/footer.php
@@ -0,0 +1,52 @@
+<!--Footer -->
+<div class="footer">
+            
+            <div class="infooter1">
+                <div class="infooter11"><h2>Top companies choose Udemy Business to build in-demand career skills.</h2></div>
+                <div class="infooter12">
+                    <div><h2>Nasdaq</h2></div>
+                    <div><h2>box</h2></div>
+                    <div><h2>NetApp</h2></div>
+                    <div><h2>eventbrite</h2></div>
+                </div>
+            </div>
+
+            <div class="infooter2">
+                <div class="infooter21">
+
+                    <div class="col">
+                        <div><a href="#">Udemy Business</a></div>
+                        <div><a href="#">Teach on Udemy</a></div>
+                        <div><a href="#">Get the app</a></div>
+                        <div><a href="#">About us</a></div>
+                        <div><a href="#">Contact us</a></div>
+                    </div>
+
+                    <div class="col">
+                        <div><a href="#">Careeers</a></div>
+                        <div><a href="#">Blogs</a></div>
+                        <div><a href="#">Help and Support</a></div>
+                        <div><a href="#">Afiliate</a></div>
+                        <div><a href="#">Investors</a></div>
+                    </div>
+
+                    <div class="col">
+                        <div><a href="#">Terms</a></div>
+                        <div><a href="#">Privacy policy</a></div>
+                        <div><a href="#">Cookie Settings</a></div>
+                        <div><a href="#">Sitemap</a></div>
+                        <div><a href="#">Accessibility Statement</a></div>
+                    </div>
+
+                </div>
+                <div class="infooter22"><h3 class="lasth3"><box-icon name='globe' style="margin-top: 5%;"></box-icon> English</h3></div>
+
+            </div>
+
+            <div class="infooter3">
+                    <div><img src="images/logo-udemy-white.svg" width="91" height="34"/></div>
+                    <div class="copy"><small> &#169; 2024 Udemy, Inc.</small></div>
+            </div>
+
+         </div>
+        <!--Footer end -->
