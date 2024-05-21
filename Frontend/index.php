3ea2d99ae5f28b302619d521cac2fa35a49d68d0
Mahider Jemere <124486265+Mahi-gitlex@users.noreply.github.com>
2024-05-22 01:10:40 +0300

    Create index.php

---
+++ Frontend/index.php
@@ -0,0 +1,249 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Font awesome icon link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- font awesome icoon link end -->
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/boxicons/2.1.0/dist/boxicons.js" integrity="sha512-Dm5UxqUSgNd93XG7eseoOrScyM1BVs65GrwmavP0D0DujOA8mjiBfyj71wmI2VQZKnnZQsSWWsxDKNiQIqk8sQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="style/main.css"/>
+    <link rel="stylesheet" href="style/header.css"/>
+    <link rel="stylesheet" href="style/footer.css"/>
+    <!-- CSS end -->
+
+    <title>Udemy</title>
+   
+</head>
+
+<body>
+    <div class="wrapper">
+
+       
+
+        <?php require "header.php";?>
+
+        
+        <!--Main section -->
+        <div class="main1">
+
+           <?php
+           if(basename($_SERVER['PHP_SELF'])=="home.php"){ echo"
+           <div class='inmain1text'>
+
+                <div class='textcontainer'>
+                    <div class='commontext development'>Development</div>
+                    <div class='commontext business2'>Business</div>
+                    <div class='commontext accounting'>Finance & Accounting</div>
+                    <div class='commontext IT'>IT & Software</div>
+                    <div class='commontext office'>Office and Productivity</div>
+                    <div class='commontext personal'>Personal Development</div>
+                    <div class='commontext design'>Design</div>
+                    <div class='commontext marketing'>Marketing</div>
+                    <div class='commontext health'>Health and Fitness</div>
+                    <div class='commontext music'>Music</div>
+                </div>
+
+            </div>";}
+            ?>
+
+           <div class="inmain">
+            
+                <div class="inmain1img">
+                     <img src="images/bodyudemy.jpg"/>
+                    </div>
+
+
+                <h1 class="what">What to learn next</h1>
+
+                <div class="videos">
+
+                    <!-- row -->
+                    <div class="video1">
+                        <div class="vidimg">
+                            <img src="images/forvideo.jpg">
+                        </div>
+                        <div class="description">
+                        <div>Learn Programming</div>
+                            <div>Alpha Team</div>
+                            <div>5 <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i> </div>
+                            <div>$100</div>
+                        </div>
+                    </div>
+
+                    <div class="video1">
+                        <div class="vidimg">
+                        <img src="images/forvideo.jpg">
+                        </div>
+                        <div class="description">
+                        <div>Learn Programming</div>
+                            <div>Alpha Team</div>
+                            <div>5 <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i> </div>
+                            <div>$100</div>
+                        </div>
+                    </div>
+
+                    <div class="video1">
+                        <div class="vidimg">
+                        <img src="images/forvideo.jpg">
+                        </div>
+                        <div class="description">
+                        <div>Learn Programming</div>
+                            <div>Alpha Team</div>
+                            <div>5 <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i> </div>
+                            <div>$100</div>
+                        </div>
+                    </div>
+
+                    <div class="video1">
+                        <div class="vidimg">
+                            <img src="images/forvideo.jpg">
+                        </div>
+                        <div class="description">
+                        <div>Learn Programming</div>
+                            <div>Alpha Team</div>
+                            <div>5 <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i> </div>
+                            <div>$100</div>
+                        </div>
+                    </div>
+
+                    <div class="video1">
+                        <div class="vidimg">
+                            <img src="images/forvideo.jpg">
+                        </div>
+                        <div class="description">
+                        <div>Learn Programming</div>
+                            <div>Alpha Team</div>
+                            <div>5 <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i> </div>
+                            <div>$100</div>
+                        </div>
+                    </div>
+                    <!--row  ends-->
+
+                     <!-- row -->
+                     <div class="video1">
+                        <div class="vidimg">
+                            <img src="images/forvideo.jpg">
+                        </div>
+                        <div class="description">
+                        <div>Learn Programming</div>
+                            <div>Alpha Team</div>
+                            <div>5 <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i> </div>
+                            <div>$100</div>
+                        </div>
+                    </div>
+
+                    <div class="video1">
+                        <div class="vidimg">
+                            <img src="images/forvideo.jpg">
+                        </div>
+                        <div class="description">
+                        <div>Learn Programming</div>
+                            <div>Alpha Team</div>
+                            <div>5 <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i> </div>
+                            <div>$100</div>
+                        </div>
+                    </div>
+
+                    <div class="video1">
+                        <div class="vidimg">
+                            <img src="images/forvideo.jpg">
+                        </div>
+                        <div class="description">
+                        <div>Learn Programming</div>
+                            <div>Alpha Team</div>
+                            <div>5 <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i> </div>
+                            <div>$100</div>
+                        </div>
+                    </div>
+
+                    <div class="video1">
+                        <div class="vidimg">
+                             <img src="images/forvideo.jpg">
+                        </div>
+                        <div class="description">
+                        <div>Learn Programming</div>
+                            <div>Alpha Team</div>
+                            <div>5 <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i> </div>
+                            <div>$100</div>
+                        </div>
+                    </div>
+
+                    <div class="video1">
+                        <div class="vidimg">
+                            <img src="images/forvideo.jpg">
+                        </div>
+                        <div class="description">
+                        <div>Learn Programming</div>
+                            <div>Alpha Team</div>
+                            <div>5 <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i> </div>
+                            <div>$100</div>
+                        </div>
+                    </div>
+                    <!--row  ends-->
+
+                     <!-- row -->
+                     <div class="video1">
+                        <div class="vidimg">
+                             <img src="images/forvideo.jpg">
+                        </div>
+                        <div class="description">
+                        <div>Learn Programming</div>
+                            <div>Alpha Team</div>
+                            <div>5 <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i> </div>
+                            <div>$100</div>
+                        </div>
+                    </div>
+
+                    <div class="video1">
+                        <div class="vidimg">
+                            <img src="images/forvideo.jpg">
+                        </div>
+                        <div class="description">
+                            <div>Learn Programming</div>
+                            <div>Alpha Team</div>
+                            <div>5 <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i> </div>
+                            <div>$100</div>
+                        </div>
+                    </div>
+
+                    
+                    <!--row  ends-->
+
+
+                </div>
+
+           </div>
+
+
+        </div>
+        <!--Main section end -->
+
+
+
+
+         <?php require "footer.php";?>
+
+
+    </div>
+    <!-- wrapper end -->
+</body>
+</html>
