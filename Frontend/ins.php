c9736f7c7cab2f720c8bc6c1f2514bf529e7b0aa
Finotmah <124761678+Finotmah@users.noreply.github.com>
2024-05-29 11:43:15 -0700

    Update ins.php

--- Frontend/ins.php
+++ Frontend/ins.php
@@ -3,7 +3,7 @@ session_start();
 $user_id =  $_SESSION['user_id'];
 $title=$video=[];
 $course_id=[];
-// Database connection parameters
+// Database connection parameter
 $servername = "localhost";
 $username = "root";
 $password = "";
